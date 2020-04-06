36b953ed1fe8af79310d271c21180333e446bb62
Hunter <hking0036@gmail.com>
2020-04-06 18:51:15 -0400

    include some big brain learning moments

--- polynomial.hs
+++ polynomial.hs
@@ -1,9 +1,10 @@
 main = do
 	putStrLn "Enter a divisor polynomial"
 	rawform <- getLine
-	let form = read rawform :: String
+	--We need to assign our input to a variable to parse, but we can use putStrLn to assume it is a string for input purposes.
+	--let form = read rawform :: String
 	putStrLn "Enter a dividend polynomial"
-	rawform <- getLine
-	let form2 = read rawform :: String
-	putStrLn(show form)
-	putStrLn(show form2)
+	rawform2 <- getLine
+	--let form2 = read rawform2 :: String
+	putStrLn(show rawform)
+	putStrLn(show rawform2)
