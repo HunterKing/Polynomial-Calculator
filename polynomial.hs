69d15c091fc34f57798468995bc7938d32169a4f
storyjss <lefoustory@gmail.com>
2020-04-30 23:10:42 -0400

    Getting there

--- polynomial.hs
+++ polynomial.hs
@@ -8,7 +8,9 @@ main = do
     rawform <- getLine
     putStrLn(show (splitString rawform))
     putStrLn(show (degreeFinder (splitString rawform)))
-    putStrLn(show (coefficient(splitString rawform)))
+    putStrLn(show (zeroListBuild (length (degreeFinder (splitString rawform)))))
+    putStrLn(show (coef (zeroListBuild (length (degreeFinder (splitString rawform)))) (splitString rawform)))
+    -- putStrLn(show (coefficient(splitString rawform)))
     
 
 splitString :: String -> [String]
@@ -18,13 +20,14 @@ degreeFinder :: [String] -> [Integer]
 degreeFinder [] = []
 degreeFinder ((x:cs):xs) = if (x == '^') then expListBuild [readNumber cs] else degreeFinder xs
 
-coefficient :: [String] -> [Integer]
-coefficient [] = []
-coefficient ((x:cs):xs)
-    | (x == '-') = readNumber (findValue (x:cs)) : coefficient xs
-    | (x == '+') = readNumber (findValue cs) : coefficient xs
-    | (isChar x) = 1 : coefficient xs
-    | otherwise = coefficient xs
+
+coef :: [Integer] -> [String] -> [Integer]
+coef [] [] = []
+coef [] [s] = []
+coef [x] [] = [x]
+coef (x:xs) ((t:ts):ss)  
+   | ((expo (t:ts)) > 0) = [0] --coef (replaceNth ((expo (t:ts)) - 1) (readNumber ts) (x:xs)) ss 
+   | otherwise = coef (x:xs) [] 
 
 -- helper Functios
 --Convert a string into an Integer value.
@@ -43,6 +46,10 @@ expListBuild :: [Integer] -> [Integer]
 expListBuild [] = [0]
 expListBuild [x] = if (x>0) then x : expListBuild [x - 1] else expListBuild []
 
+zeroListBuild :: Int -> [Integer]
+zeroListBuild 0 = []
+zeroListBuild x = if (x>0) then 0 : zeroListBuild (x-1) else zeroListBuild 0  
+
 isNum :: Char -> Bool
 isNum c = '0' <= c && c <= '9'
 
@@ -54,3 +61,13 @@ filterNot pred = filter $ not . pred
 
 removeEmpty :: [String] -> [String]
 removeEmpty x = filterNot (`elem` [""]) x
+
+expo :: String -> Integer
+expo [] = 0
+expo (s:ss) = if (s == '^') then readNumber ss else expo ss
+
+replaceNth :: Integer -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth n newVal (x:xs)
+  | n == 0 = newVal:xs
+  | otherwise = x:replaceNth (n-1) newVal xs
