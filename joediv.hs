8c80715ba1bee81c5a977cc3f5b95b490f82b88c
storyjss <lefoustory@gmail.com>
2020-05-03 00:12:02 -0400

    MULTIPLICATION PROGRESS!

--- joediv.hs
+++ joediv.hs
@@ -1,5 +1,6 @@
 combineEqs :: [(Integer,Integer)] -> [(Integer,Integer)]
-comineEqs [] = []
+combineEqs [] = []
+combineEqs [x] = x : combineEqs []
 combineEqs ((x1,x2):(y1,y2):rest) 
    | (x2 == y2) = ((x1 + y1), x2) : rest
    | otherwise = (x1, x2) : combineEqs ((y1,y2):rest)
@@ -12,18 +13,19 @@ hasDuplicate ((x1,x2):(y1,y2):rest)
    | otherwise = (x1,x2) : hasDuplicate ((y1,y2):rest) 
 
 -- multiplyEqs :: [(Integer,Integer)] -> [(Integer,Integer)] -> [(Integer,Integer)]
--- multiplyEqs [] y = []
--- multiplyEqs [x] [] = x : multiplyEqs [] []
--- multiplyEqs ((x1,x2):rest1) ((y1,y2):rest2) 
---    | (null ((x1,x2):rest1)) = multiplyEqs [] ((y1,y2):rest2) 
---    | otherwise = (x1 * y1,x2 + y2) : multiplyEqs rest1 ((y1,y2):rest2) 
+-- -- multiplyEqs [] y = []
+-- multiplyEqs [x] y = x : []
+-- multiplyEqs ((x1,x2):rest) y 
+--    | (not (null (x1,x2))) = a ++ (multiplyEqs rest y)
+--    | otherwise = multiplyEqs [] y
+--       where a = (zip (map (x1*) (splitListC y)) (map (x2+) (splitListP y)))
 
 multiplyEqs :: [(Integer,Integer)] -> [(Integer,Integer)] -> [(Integer,Integer)]
-multiplyEqs [] y = []
-multiplyEqs [x] [] = [] 
-multiplyEqs [x] y = x : multiplyEqs [] y
-multiplyEqs ((x1,x2):rest) y = a : multiplyEqs rest a
-    where a = zip (map (x1*) (splitListC y)) (map (x2+) (splitListP y))
+multiplyEqs [(x1,x2)] y = combineEqs (zip (map (x1*) (splitListC y)) (map (x2+) (splitListP y))) ++ []
+multiplyEqs ((x1,x2):rest) y = combineEqs (zip (map (x1*) (splitListC y)) (map (x2+) (splitListP y))) ++ (multiplyEqs rest y)
+
+
+
 
 splitListC :: [(Integer,Integer)] -> [Integer]
 splitListC [] = []
